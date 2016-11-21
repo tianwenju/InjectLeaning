e003c604577097be4673e44a8835d46bf5cfeac9
V.Wenju.Tian <V.Wenju.Tian@deltaww.com>
2016-11-21 13:17:28 +0800

    基本反射

--- app/src/main/java/com/wenjutian/injectleaning/InjectInvocationHandler.java
+++ app/src/main/java/com/wenjutian/injectleaning/InjectInvocationHandler.java
@@ -11,7 +11,7 @@ import java.util.Map;
  * Created by V.Wenju.Tian on 2016/11/18.
  */
 public class InjectInvocationHandler implements InvocationHandler {
-   // 拦截的方法名列表,也就是加了Onclick
+    // 拦截的方法名列表,里面存的就是我们注解的方法名字,关键字是OnClick
     private Map<String, Method> map = new HashMap<>();
     //    在这里实际上是MainActivity
     private Object target;
@@ -23,11 +23,12 @@ public class InjectInvocationHandler implements InvocationHandler {
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
         if (target != null) {
-//            获取方法名
+//      获取方法名,因为我们是要截获OnClick方法,所以我们要拿到OnClick方法
             String name = method.getName();
-            Log.e("自定义标签", "invoke: "+method.getName());
+            Log.e("自定义标签", "invoke: " + method.getName());
             Method m = map.get(name);
-            Log.e("自定义标签", "invoke: activity"+m.getName());
+            //如果method的方法名字是onClick,我们就能取得我们注解的方法,从而直接调用
+            Log.e("自定义标签", "invoke: activity" + m.getName());
             //其实调用的是Activity的加了Onclick的方法
             if (m != null) {
                 return m.invoke(target, args);
--- app/src/main/java/com/wenjutian/injectleaning/InjectUtls.java
+++ app/src/main/java/com/wenjutian/injectleaning/InjectUtls.java
@@ -13,15 +13,107 @@ import java.lang.reflect.Proxy;
  */
 
 public class InjectUtls {
-
-
     public static void inject(Activity activity) {
-        injectView(activity);
-        InjectOnClick(activity);
+        injects(activity);
+        //非动态代理实现
+        // InjectOnClick(activity);
+        //动态代理实现
+        InjectProxyOnClick(activity);
     }
+//    @Target(ElementType.FIELD)
+//    @Retention(RetentionPolicy.RUNTIME)
+//    public @interface InjectView {
+//        int value();
+//    }
+    // activity.findViewById(id);
+    //@InjectView(R.id.text)
+    //TextView textView;
 
-    private static void InjectOnClick(Activity activity) {
+    /**
+     * 对于注解字段的,是要实现字段的赋值,怎么复制呢?
+     * 1.实例化对象
+     * 1.1  通过拿字段的注解我们能够拿到id
+     * 1.2 findviewById()
+     * 2.对象复制
+     * 2.1  declaredField.set(activity,viewById);
+     *
+     * @param activity
+     */
+    public static void injects(Activity activity) {
 
+        Class<? extends Activity> aClass = activity.getClass();
+
+        Field[] declaredFields = aClass.getDeclaredFields();
+        for (Field declaredField : declaredFields) {
+
+            declaredField.setAccessible(true);
+            if (declaredField.isAnnotationPresent(InjectView.class)) {
+                declaredField.setAccessible(true);
+                InjectView annotation = declaredField.getAnnotation(InjectView.class);
+                int value = annotation.value();
+                View viewById = activity.findViewById(value);//当然也可以是反射
+                try {
+                    declaredField.set(activity, viewById);
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
+                }
+
+
+            }
+        }
+
+    }
+    /**
+     * 注解事件
+     * 原理:我们知道点击事件的监听.是通过 view.setOnclickListener(new onClickListener())的方法实现的,问题是我们怎么调用
+     * 注解事件的方法
+     * 1.拿到对象View
+     * 2.实现点击事件
+     *
+     * @param activity
+     */
+    private static void injectOnlick(final Activity activity) {
+        Class<? extends Activity> aClass = activity.getClass();
+        Method[] declaredMethods = aClass.getDeclaredMethods();
+        for (final Method declaredMethod : declaredMethods) {
+            {
+
+                OnClick annotation = declaredMethod.getAnnotation(OnClick.class);
+                if (annotation != null) {
+                    int[] value = annotation.value();
+                    for (int j : value) {
+                        final View viewById = activity.findViewById(j);
+
+                        viewById.setOnClickListener(new View.OnClickListener() {
+                            private Method method;
+
+                            @Override
+                            public void onClick(View v) {
+                                try {
+                                    declaredMethod.invoke(activity, v);
+                                } catch (IllegalAccessException e) {
+                                    e.printStackTrace();
+                                } catch (InvocationTargetException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        });
+                    }
+                }
+
+
+            }
+        }
+    }
+    //动态代理实现方式
+    /**
+     * 1.拿到View对象
+     * 2.监听对象我们可以用动态代理实现.通过代理View.OnClickListener对象,当触发onClick事件的时候调用InjectInvocationHandler的Invoke方法,从而我们可以再Invoke方法中调用
+     * 我们注解的方法
+     *
+     * @param activity
+     */
+    private static void InjectProxyOnClick(Activity activity) {
         Class<? extends Activity> aClass = activity.getClass();
         Method[] declaredMethods = aClass.getDeclaredMethods();
         for (Method declaredMethodd : declaredMethods) {
@@ -58,39 +150,36 @@ public class InjectUtls {
 
     }
 
-    private static void injectView(Activity activity) {
-
-        //1.拿到aClass对象
-        Class<? extends Activity> aClass = activity.getClass();
-        //2.我们控件声明一般是字段,那么我们就要拿到字段
-        Field[] declaredFields = aClass.getDeclaredFields();
-        for (Field declaredField : declaredFields) {
-            if (declaredField.isAnnotationPresent(InjectView.class)) {
-                //3 .拿到InjectView注解的值
-                InjectView annotation = declaredField.getAnnotation(InjectView.class);
-                int id = annotation.value();
-                try {
-                    //4.用activity.findViewById方法,返回我们要的那个View
-                    Method findViewById = aClass.getMethod("findViewById", int.class);
-
-                    Object view = findViewById.invoke(activity, id);
-                    declaredField.setAccessible(true);
-                    //然后在给activity的字段赋值
-                    declaredField.set(activity, view);
-                } catch (NoSuchMethodException e) {
-                    e.printStackTrace();
-                } catch (InvocationTargetException e) {
-                    e.printStackTrace();
-                } catch (IllegalAccessException e) {
-                    e.printStackTrace();
-                }
-
-            }
-        }
-
-    }
-
-    private static void injectViews(Activity activity) {
-
-    }
+//    private static void injectView(Activity activity) {
+//
+//        //1.拿到aClass对象
+//        Class<? extends Activity> aClass = activity.getClass();
+//        //2.我们控件声明一般是字段,那么我们就要拿到字段
+//        Field[] declaredFields = aClass.getDeclaredFields();
+//        for (Field declaredField : declaredFields) {
+//            if (declaredField.isAnnotationPresent(InjectView.class)) {
+//                //3 .拿到InjectView注解的值
+//                InjectView annotation = declaredField.getAnnotation(InjectView.class);
+//                int id = annotation.value();
+//                try {
+//                    //4.用activity.findViewById方法,返回我们要的那个View
+//                    Method findViewById = aClass.getMethod("findViewById", int.class);
+//
+//                    Object view = findViewById.invoke(activity, id);
+//                    declaredField.setAccessible(true);
+//                    //然后在给activity的字段赋值
+//                    declaredField.set(activity, view);
+//                } catch (NoSuchMethodException e) {
+//                    e.printStackTrace();
+//                } catch (InvocationTargetException e) {
+//                    e.printStackTrace();
+//                } catch (IllegalAccessException e) {
+//                    e.printStackTrace();
+//                }
+//
+//            }
+//        }
+//
+//    }
+//
 }
